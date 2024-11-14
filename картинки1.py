471a1d605255eecf5c3ace7d94cefe6035caa2ed
Anna <amkalashnikova96@gmail.com>
2024-11-14 16:02:47 +0300

    Определена функция для отправки запроса и преобразовании ответа в нужный формат

--- картинки1.py
+++ картинки1.py
@@ -2,14 +2,21 @@ from cProfile import label
 from tkinter import *
 import requests
 from PIL import Image, ImageTk
-from io import bytesIO
+from io import BytesIO
 
 from bottle import response
 from pygame.examples.aliens import load_image
+from pygame.examples.cursors import image
+
 
 def load_image(url):
-    response=requests.get()
+    response=requests.get(url)
     print(response)
+    image_data=BytesIO(response.content)
+    print(response.headers)
+    img=Image.open(image_data)
+    print(img)
+    return ImageTk.PhotoImage(img)
 
 w=Tk()
 w.geometry("500x500")
@@ -23,5 +30,8 @@ img=load_image(url)
 if img:
     label.config(image=img)
     label.image=img
+
+but=Button(text="Обновить картинку", command=load_image)
+but.pack()
 
 w.mainloop()
